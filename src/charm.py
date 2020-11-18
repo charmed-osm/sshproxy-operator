bf1f8989be4951c6055c9703050cc39cbbac0ac8
David Garcia <david.garcia@canonical.com>
2020-11-18 17:54:42 +0100

    Initial commit

---
+++ src/charm.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# Copyright 2020 David Garcia
+# See LICENSE file for licensing details.
+
+from ops.main import main
+
+from charms.osm.sshproxy import SSHProxyCharm
+from ops.model import (
+    ActiveStatus,
+    MaintenanceStatus,
+)
+
+
+class SshproxyCharm(SSHProxyCharm):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        # An example of setting charm state
+        # that's persistent across events
+        self.state.set_default(is_started=False)
+
+        if not self.state.is_started:
+            self.state.is_started = True
+
+        # Register all of the events we want to observe
+        # Charm events
+        self.framework.observe(self.on.config_changed, self.on_config_changed)
+        self.framework.observe(self.on.install, self.on_install)
+        self.framework.observe(self.on.start, self.on_start)
+        self.framework.observe(self.on.upgrade_charm, self.on_upgrade_charm)
+        # Charm actions (primitives)
+        self.framework.observe(self.on.touch_action, self.on_touch_action)
+        # OSM actions (primitives)
+        self.framework.observe(self.on.start_action, self.on_start_action)
+        self.framework.observe(self.on.stop_action, self.on_stop_action)
+        self.framework.observe(self.on.restart_action, self.on_restart_action)
+        self.framework.observe(self.on.reboot_action, self.on_reboot_action)
+        self.framework.observe(self.on.upgrade_action, self.on_upgrade_action)
+
+    def on_config_changed(self, event):
+        """Handle changes in configuration"""
+        super().on_config_changed(event)
+
+    def on_install(self, event):
+        super().on_install(event)
+
+    def on_start(self, event):
+        """Called when the charm is being installed"""
+        super().on_start(event)
+
+    def on_upgrade_charm(self, event):
+        """Upgrade the charm."""
+        self.unit.status = MaintenanceStatus("Upgrading charm")
+        # Do upgrade related stuff
+        self.unit.status = ActiveStatus("Active")
+
+    def on_touch_action(self, event):
+        """Touch a file."""
+
+        if self.unit.is_leader():
+            filename = event.params["filename"]
+            proxy = self.get_ssh_proxy()
+            stdout, stderr = proxy.run("touch {}".format(filename))
+            event.set_results({"output": stdout})
+        else:
+            event.fail("Unit is not leader")
+            return
+
+    ###############
+    # OSM methods #
+    ###############
+    def on_start_action(self, event):
+        """Start the VNF service on the VM."""
+        pass
+
+    def on_stop_action(self, event):
+        """Stop the VNF service on the VM."""
+        pass
+
+    def on_restart_action(self, event):
+        """Restart the VNF service on the VM."""
+        pass
+
+    def on_reboot_action(self, event):
+        """Reboot the VM."""
+        if self.unit.is_leader():
+            proxy = self.get_ssh_proxy()
+            stdout, stderr = proxy.run("sudo reboot")
+            if len(stderr):
+                event.fail(stderr)
+        else:
+            event.fail("Unit is not leader")
+            return
+
+    def on_upgrade_action(self, event):
+        """Upgrade the VNF service on the VM."""
+        pass
+
+
+if __name__ == "__main__":
+    main(SshproxyCharm)
